8c8f217dbb832d2ef8819a7e420572ef2b4c04df
Olekciy <0lekciy4@gmail.com>
2018-02-28 23:13:24 +0300

    Sloving 6_password_strength

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # Password Strength Calculator
+You can check password strength.
 
-[TODO. There will be project description]
+Example run:
+
+```bash
+$ python3 password_strength.py
+Input your password for check strenght:
+Password strength = 10
+```
+
+Password is evaluated by:
+- Lowercase letters
+- Uppercase letters
+- Digits
+- Special characters
+- Length
+- Duplicate characters
+- Presence in the dictionary
+
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,57 @@
+# [TODO] отправлять введенные пароли
+
+import re
+from getpass import getpass
+
+
+def load_bad_passwords(file_path):
+    with open(file_path, 'r') as file_handler:
+        return file_handler.read().split(sep='\n')
+
+
+def check_in_bad_passwords_list(password):
+    bad_passwords_list = load_bad_passwords('bad_passwords.txt')
+    if password.lower() not in bad_passwords_list:  # т.к. в словаре только строчные символы
+        return 1
+    return -3
+
+
+def check_in_patterns(password):
+    password_in_pattern = 0
+    for pattern in ('[a-z]', '[A-Z]', '\d', '\W'):
+        if re.findall(pattern, password):
+            password_in_pattern += 1
+    return password_in_pattern
+
+
+def check_len(password):
+    if len(password) >= 8:
+        return 4
+    elif len(password) >= 6:
+        return 2
+    else:
+        return -2
+
+
+def check_for_duplicate_characters(password):
+    if len(password) == len(set(password)):
+        return 1
 
 
 def get_password_strength(password):
-    pass
+    password_strength = 0
+    check_password = [
+        check_for_duplicate_characters(password),
+        check_in_bad_passwords_list(password),
+        check_in_patterns(password),
+        check_len(password),
+    ]
+    for i in check_password:
+        if i is not None:
+            password_strength += i
+    return password_strength
 
 
 if __name__ == '__main__':
-    pass
+    password = getpass('Input your password for check strenght:')
+    print('Password strength = {}'.format(get_password_strength(password)))
